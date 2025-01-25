81e42717fe0a7212b946d1a2410491efa3c5c7c7
Tatu Laras <tatu.laras@gmail.com>
2025-01-25 17:07:52 +0200

    CSS styles are back

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-// import 'normalize.css';
-// import './index.css';
-// import './Navbar.css';
-// import './Hero.css';
-// import './Projects.css';
-// import './ProjectItem.css';
-// import './CV.css';
-// import './ArrowButton.css';
-// import './Button.css';
-// import './Contact.css';
-// import './Footer.css';
-// import './ProjectDetails.css';
-// import './Images.css';
+import 'normalize.css';
+import './index.css';
+import './Navbar.css';
+import './Hero.css';
+import './Projects.css';
+import './ProjectItem.css';
+import './CV.css';
+import './ArrowButton.css';
+import './Button.css';
+import './Contact.css';
+import './Footer.css';
+import './ProjectDetails.css';
+import './Images.css';
 
 import App from './App.tsx';
 
