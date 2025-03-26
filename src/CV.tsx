5eae63cb1358b6bb21c3a660f9d7f428c8ea3936
Tatu Laras <tatu.laras@gmail.com>
2025-03-26 14:33:35 +0200

    Add CV download link

--- src/CV.tsx
+++ src/CV.tsx
@@ -5,7 +5,7 @@ export default function CV() {
     return (
         <section id="cv">
             <h1>CV</h1>
-            <Button text="Download CV" />
+            <Button text="Download CV" link="/downloadable/cv_en.pdf" />
             <ArrowButton text="Contact me" link="#contact" />
         </section>
     );
